cc0393daf8b7b7885957797fdc9beaebcd8ce6bf
7Teh <kianoraditya1@gmail.com>
2026-06-21 09:09:45 +0700

    Add files via upload

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,48 +1,21 @@
-const CACHE_NAME = 'nonogram-solver-v1';
-const urlsToCache = [
-  '.',
-  'index.html',
-  'manifest.json',
-  'launchericon-512x512.png',
-  'launchericon-192x192.png'
-];
+const CACHE_NAME = 'nonogram-v1';
 
-// Install Service Worker
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '.',
+        'index.html',
+        'manifest.json'
+      ]);
+    })
   );
 });
 
-// Fetch (ambil dari cache)
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-// Activate (update cache)
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
